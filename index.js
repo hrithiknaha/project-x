ab5bd4ba957303991e2e95fcceab5aafbaf10c55
Hrithik Naha <naha.hrithkk@gmail.com>
2020-04-18 15:58:23 +0530

    Add dotenv

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 const indexRoutes = require('./routes/index');
 const writeRoutes = require('./routes/write');
 
 const app = express();
+dotenv.config();
+
+mongoose.connect(
+	process.env.DB_CONNECT,
+	{ useNewUrlParser: true, useUnifiedTopology: true },
+	(err) => {
+		if (err) return console.log('Can not connect to the database');
+		console.log('DB Connected');
+	}
+);
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
