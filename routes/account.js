78b51c0c41e6b975534182a4fdab74bd01393650
Hrithik Naha <naha.hrithkk@gmail.com>
2020-04-23 19:21:05 +0530

    Added Account route

--- routes/account.js
+++ routes/account.js
@@ -2,8 +2,7 @@ const router = require('express').Router({ mergeParams: true });
 const User = require('../models/Users');
 
 router.get('/', (req, res) => {
-	console.log('Hello');
-	res.send('hello');
+	res.render('account/index', { user: req.params.username });
 });
 
 module.exports = router;
