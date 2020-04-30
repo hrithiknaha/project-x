49e601e3ce9d39a37c1f30db90c1a5058a0f71bf
Hrithik Naha <naha.hrithkk@gmail.com>
2020-04-30 16:04:38 +0530

    Accound style one

--- routes/account.js
+++ routes/account.js
@@ -4,13 +4,11 @@ const Journals = require('../models/Journals');
 
 const { isLoggedIn } = require('../middleware/index');
 
-router.get('/', (req, res) => {
-	Journals.find(
-		{ 'author.id': '5e9c7e9b2c84836960c9d3b4' },
-		(err, journals) => {
-			res.render('account/index', { user: 'hrithik naha', journals });
-		}
-	);
+router.get('/', isLoggedIn, (req, res) => {
+	Journals.find({ 'author.id': req.user._id }, (err, journals) => {
+		res.render('account/index', { user: req.user, journals });
+		console.log(req.user);
+	});
 });
 
 module.exports = router;
