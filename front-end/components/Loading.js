b48ddfc5a0bd41878fb5e43c7280539398d1c86f
Hrithik Naha <naha.hrithkk@gmail.com>
2020-05-27 20:32:51 +0530

    Loading changed

--- front-end/components/Loading.js
+++ front-end/components/Loading.js
@@ -4,11 +4,6 @@ const Loading = (props) => {
 	return (
 		<div className='loading'>
 			<p className='loading-text'>Wait, your content is loading</p>
-			<div className='d-flex justify-content-center'>
-				<div className='spinner-border loading-main' role='status'>
-					<span className='sr-only'>Loading...</span>
-				</div>
-			</div>
 		</div>
 	);
 };
